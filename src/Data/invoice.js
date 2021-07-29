c85717f9cff615467d3652bf6b0e06e85daa7a9a
Eashan Banerjee <55328960+Ph1Eashan@users.noreply.github.com>
2021-07-29 14:22:19 +0530

    Add files via upload

---
+++ src/Data/invoice.js
@@ -0,0 +1,45 @@
+const invoice = {
+  id: '61018d17a4bbcf77247fc78a',
+  invoice_no: '202009-15',
+  balance: '$2,274.10',
+  company: 'SKYPLEX',
+  email: 'buckleychan@skyplex.com',
+  phone: '+1 (916) 494-2014',
+  address: '767 Rost Place, Coultervillle, Texas, 7210',
+  trans_date: '2019-09-21',
+  due_date: '2019-10-13',
+  items: [
+    {
+      sno: 1,
+      desc: 'velit consequat cillum sit anim',
+      qty: 9,
+      rate: 81.81,
+    },
+    {
+      sno: 2,
+      desc: 'adipisicing nostrud et consequat nisi',
+      qty: 4,
+      rate: 461.6,
+    },
+    {
+      sno: 3,
+      desc: 'commodo amet aliquip deserunt Lorem',
+      qty: 2,
+      rate: 948.83,
+    },
+    {
+      sno: 4,
+      desc: 'ullamco fugiat non ipsum nulla',
+      qty: 5,
+      rate: 186.47,
+    },
+    {
+      sno: 5,
+      desc: 'id culpa irure reprehenderit aliquip',
+      qty: 4,
+      rate: 14.98,
+    },
+  ],
+};
+
+export default invoice;
